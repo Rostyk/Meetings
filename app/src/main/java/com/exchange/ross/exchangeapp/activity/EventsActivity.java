897496441a3f3a9f3a8c6dfee244ee032a8e56f5
Rostyslav Stepanyak <ros.aphex@gmail.com>
2015-04-20 17:29:37 +0300

    add settings screen placeholder

--- app/src/main/java/com/exchange/ross/exchangeapp/activity/EventsActivity.java
+++ app/src/main/java/com/exchange/ross/exchangeapp/activity/EventsActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.exchange.ross.exchangeapp.R;
@@ -61,6 +63,15 @@ public class EventsActivity extends ActionBarActivity implements EventsFragment.
         //mViewPager.setOffscreenPageLimit(100);
         mViewPager.setOnPageChangeListener(pagerAdapter);
         mViewPager.setAdapter(pagerAdapter);
+
+        View settingsVew = findViewById(R.id.settingsButtonView);
+        settingsVew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingsActivityIntent = new Intent(EventsActivity.this, SettingsActivity.class);
+                EventsActivity.this.startActivity(settingsActivityIntent);
+            }
+        });
         //Start time service
         startService(new Intent(this, TimeService.class));
     }
@@ -88,18 +99,7 @@ public class EventsActivity extends ActionBarActivity implements EventsFragment.
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent settingsActivityIntent = new Intent(EventsActivity.this, SettingsActivity.class);
-            EventsActivity.this.startActivity(settingsActivityIntent);
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
